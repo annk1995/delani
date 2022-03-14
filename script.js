be9319d8505067c19eb9e15cd37f2eb169cc18b0
annk1995 <kabuikariuki052@gmail.com>
2022-03-14 13:45:47 +0300

    added styles to the portfolio page

--- script.js
+++ script.js
@@ -15,4 +15,36 @@ $(document).ready(function() {
         $('#textarea').val('');
     });
   });
- 
+  $(document).ready(function() {
+    $('#title1').hide();
+    $('#title2').hide();
+    $('#title3').hide();
+    $('#desiPara').hide();
+    $('#devPara').hide();
+    $('#proPara').hide();
+  });
+  
+  $(document).ready(function() {
+    $('#design').click(function() {
+        $('img#icon1').toggle();
+        $('label#icon1').toggle();
+        $('h4#title1').toggle();
+        $('p#desiPara').toggle();
+    });
+  });
+  $(document).ready(function() {
+    $('#dev').click(function() {
+        $('img#icon2').toggle();
+        $('label#icon2').toggle();
+        $('h4#title2').toggle();
+        $('p#devPara').toggle();
+    });
+  });
+  $(document).ready(function() {
+    $('#product').click(function() {
+        $('img#icon3').toggle();
+        $('label#icon3').toggle();
+        $('h4#title3').toggle();
+        $('p#proPara').toggle();
+    });
+  });
